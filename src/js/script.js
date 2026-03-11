b5a52201687463f693b53369144b25d0328ec209
hayatcodess <halmubarak26@aoiths.org>
2026-03-11 14:10:34 -0400

    ded

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,7 @@ var Database = {
 function loadDataSource() {
 	var localData = localStorage.getItem(Database.keyName);
 	var parsedData = JSON.parse( localData);
-	Database.data = pasedData;
+	Database.data = parsedData;
 	displayData( Database.data );
 }
 
@@ -17,9 +17,33 @@ function displayData(dataArray) {
 console.log(dataArray.response);
 var root = dataArray.response;
 	root.forEach(function(value, index){
-		var html = '//add the  card layout here';
+		var html = 
+		 `<div class ="card shadow-lg">
+          <img>
+          <div class="card-body">
+            <h5 class="card-title text-center mb-3"></h5>
+            <p class="card-text text-center text-muted"></p>
+
+            <h6 class="fw-bold"></h6>
+            <ul class="list-unstyled">
+              <li></li>
+            </ul>
+
+            <h6 class="fw-bold"></h6>
+            <ul class="list-unstyled">
+              <li></li>
+            </ul>
+
+            <h6 class="fw-bold"></h6>
+            <ul class="list-inline">
+              <li class="list-inline-item badge bg-primary"></li>
+            </ul>
+          </div>
+		  </div>`;
 	
 	document.querySelector(".col").innerHTML += html;
-});
+	});
+	
+};
 
 loadDataSource();
