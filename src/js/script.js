6308788d6b46e808d0062b6a03c476baa82c0750
Hayat <144457693+hayatcodess@users.noreply.github.com>
2026-03-11 13:58:38 -0400

    Pelzers Help
    Pelzer helped us with using localStorage and displaying it

--- src/js/script.js
+++ src/js/script.js
@@ -5,16 +5,21 @@ var Database = {
 
 
 function loadDataSource() {
-
-	
+	var localData = localStorage.getItem(Database.keyName);
+	var parsedData = JSON.parse( localData);
+	Database.data = pasedData;
 	displayData( Database.data );
 }
 
 
 
 function displayData(dataArray) {
-
-
-}
+console.log(dataArray.response);
+var root = dataArray.response;
+	root.forEach(function(value, index){
+		var html = '//add the  card layout here';
+	
+	document.querySelector(".col").innerHTML += html;
+});
 
 loadDataSource();
